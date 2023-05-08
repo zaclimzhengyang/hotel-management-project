9fe9475b59bfff044e5d743035550b61ce589dd0
limzhengyang <zhengyanglim057@gmail.com>
2023-05-08 21:52:26 +0800

    new commit

--- src/main/java/com/example/hotelManageMent/amenity/AmenityMain.java
+++ src/main/java/com/example/hotelManageMent/amenity/AmenityMain.java
@@ -17,45 +17,49 @@ public class AmenityMain {
         }
     }
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException, Exception {
         Scanner scanner = null;
         ArrayList<Amenity> amenities = new ArrayList<Amenity>();
         double totalArea;
         double totalPrice;
-        try {
-            totalArea = 0;
-            totalPrice = 0;
+        totalArea = 0;
+        totalPrice = 0;
 
-            Resource resource = new ClassPathResource("SharedAmenity.txt");
-            File file = resource.getFile();
-            List<String> allLines = convertResourceToList(resource);
-            for (int i = 1; i < 3; i++) {
-                String[] firstLineValues = allLines.get(i).split(",");
-                String itemCode = firstLineValues[0];
-                String description = firstLineValues[1];
-                double price = Double.parseDouble(firstLineValues[2]);
-                totalPrice += price;
-                SharedAmenity currentAmenity = new SharedAmenity(itemCode, description, price);
-                amenities.add(currentAmenity);
+        FileReader fileReader = new FileReader("SharedAmenity.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        bufferedReader.readLine();
+        while (true) {
+            String line = bufferedReader.readLine();
+            if (line == null) {
+                break;
             }
+            String[] lineSplit = line.split(",");
+            String itemCode = lineSplit[0];
+            String description = lineSplit[1];
+            double price = Double.parseDouble(lineSplit[2]);
+            totalPrice += price;
+            SharedAmenity currentAmenity = new SharedAmenity(itemCode, description, price);
+            amenities.add(currentAmenity);
+        }
 
-            Resource resource2 = new ClassPathResource("InRoomAmenity.txt");
-            File file2 = resource2.getFile();
-            List<String> allLines2 = convertResourceToList(resource2);
-            for (int j = 1; j < 3; j++) {
-                String[] firstLineValues = allLines2.get(j).split(",");
-                String itemCode2 = firstLineValues[0];
-                String description2 = firstLineValues[1];
-                double price2 = Double.parseDouble(firstLineValues[2]);
-                double floorArea2 = Double.parseDouble(firstLineValues[3]);
-                totalPrice += price2;
-                totalArea += floorArea2;
-                SharedAmenity currentAmenity = new SharedAmenity(itemCode2, description2, price2);
-                amenities.add(currentAmenity);
+        FileReader fileReader2 = new FileReader("inRoomAmenity.txt");
+        BufferedReader bufferedReader2 = new BufferedReader(fileReader2);
+        bufferedReader2.readLine();
+        while (true) {
+            bufferedReader2.readLine();
+            String line2 = bufferedReader2.readLine();
+            if (line2 == null) {
+                break;
             }
-        } catch (FileNotFoundException e) {
-            System.out.println("Error while populating list of amenities");
-            throw new RuntimeException(e);
+            String[] lineSplit2 = line2.split(",");
+            String itemCode2 = lineSplit2[0];
+            String description2 = lineSplit2[1];
+            double price2 = Double.parseDouble(lineSplit2[2]);
+            double floorArea2 = Double.parseDouble(lineSplit2[3]);
+            totalPrice += price2;
+            totalArea += floorArea2;
+            SharedAmenity currentAmenity = new SharedAmenity(itemCode2, description2, price2);
+            amenities.add(currentAmenity);
         }
 
         for (Amenity currentAmenity : amenities) {
--- src/main/java/com/example/hotelManageMent/hotel/Hotel.java
+++ src/main/java/com/example/hotelManageMent/hotel/Hotel.java
@@ -1,10 +1,15 @@
 package com.example.hotelManageMent.hotel;
 
 import com.example.hotelManageMent.amenity.Amenity;
+import com.example.hotelManageMent.amenity.InRoomAmenity;
 import com.example.hotelManageMent.exception.BookingException;
 import com.example.hotelManageMent.guest.Guest;
 
 import java.awt.print.Book;
+import java.io.*;
+import java.nio.Buffer;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 public class Hotel {
@@ -12,9 +17,14 @@ public class Hotel {
     private HashMap<String, Guest> guests; // key: passport no, value: Guest object
     private ArrayList<Amenity> amenities;
     private HashMap<String,Booking> bookings; // key: booking ID, value: Booking object
-    private HashMap<Date,ArrayList<String>> roomAvailability; // key: date, value: standard room count and deluxe room count
-
-    public Hotel (String name, String roomFilename) {
+    private HashMap<Date,ArrayList<Integer>> roomAvailability; // key: date, value: standard room count and deluxe room count
+    /**
+     * <dd-mon-yyyy>,<standard rooms available>,<deluxe rooms available>
+     * {
+     *     "2023-05-01": [10, 10]
+     * }
+     */
+    public Hotel (String name, String roomFilename) throws IOException {
         this.name = name;
         setupGuest();
         setupAmenities();
@@ -51,7 +61,7 @@ public class Hotel {
         return true;
     }
 
-    public String listAmeinity () {
+    public String listAmenity () {
         StringBuilder sb = new StringBuilder();
         if (amenities.size() == 0) {
             return "";
@@ -93,13 +103,12 @@ public class Hotel {
 
     public void submitBooking (Booking newBooking) {
         try {
-            if (newBooking.getStatus() != "Pending") {
+            if (!Objects.equals(newBooking.getStatus(), "Pending")) {
                 throw new BookingException("Status Is Not Pending");
             }
         } catch (BookingException e) {
             System.out.println(e.getMessage());
         }
-        // TODO: check there should be check-in to check-out date, for the room type
         Date newBookingCheckInDate = newBooking.getCheckInDate();
         Date newBookingCheckOutDate = newBooking.getCheckOutDate();
         try {
@@ -112,7 +121,28 @@ public class Hotel {
         // TODO: update room availability by deducting the room type count from check-in to check-out dates
         ArrayList<Date> datesToCheck = getDatesBetweenStartAndEnd(newBookingCheckInDate,newBookingCheckOutDate);
         for (Date currDate: datesToCheck) {
-
+            ArrayList<Integer> standardDeluxeRoom = roomAvailability.get(currDate);
+            Integer standardRoom = standardDeluxeRoom.get(0);
+            Integer deluxeRoom = standardDeluxeRoom.get(1);
+            String bookingRoom = newBooking.getRoomType();
+            try {
+                if (bookingRoom.equals("Standard")) {
+                    if (standardRoom == 0) {
+                        throw new BookingException("No Standard Room Available To Be Deducted");
+                    } else {
+                        standardRoom--;
+                    }
+                }
+                if (bookingRoom.equals("Deluxe")) {
+                    if (deluxeRoom == 0) {
+                        throw new BookingException("No Deluxe Room Available To Be Deducted");
+                    } else {
+                        deluxeRoom--;
+                    }
+                }
+            } catch (BookingException e) {
+                System.out.println(e.getMessage());
+            }
         }
         newBooking.setStatus("Confirmed");
         String newBookingId = newBooking.getBookingId();
@@ -130,7 +160,7 @@ public class Hotel {
         Booking toCancelBooking = bookings.get(bookingId);
         String toCancelBookingStatus = toCancelBooking.getStatus();
         try {
-            if (toCancelBookingStatus != "Confirmed") {
+            if (!Objects.equals(toCancelBookingStatus, "Confirmed")) {
                 throw new BookingException("Booking Cannot Be Cancelled");
             }
         } catch (BookingException e) {
@@ -180,15 +210,106 @@ public class Hotel {
         return calendar;
     }
 
-    public void setupGuest() {
+    public void setupGuest() throws IOException {
         // setup Guest objects into the dictionary by reading the information from "Guests.txt" and "Blacklist.txt"
+        FileReader fileReader = new FileReader("guests.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        bufferedReader.readLine();
+        this.guests = new HashMap<>();
+        while (true) {
+            String line = bufferedReader.readLine();
+            if (line == null) {
+                break;
+            }
+            String[] splitLine = line.split(",");
+            String guestPassport = splitLine[0];
+            String guestName = splitLine[1];
+            String guestCountry = splitLine[2];
+            this.guests.put(guestPassport, new Guest(guestPassport, guestName, guestCountry));
+//            System.out.println(Arrays.toString(splitLine));
+        }
+        System.out.println(this.guests);
+
     }
 
-    public void setupAmenities() {
-        // setup the current set of Amenity objects into a list using the information provided in Appendix A1 and A2
+    public static void main(String[] args) throws IOException {
+        Hotel hotel = new Hotel("testName", "testNo");
+        hotel.saveRoomAvailability();
     }
 
-    public void saveRoomAvailability() {
-        // with the roomFilename, setup the room availability as shown in appendix c
+    public void setupAmenities() throws IOException {
+        FileReader fileReader = new FileReader("InRoomAmenity.txt");
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        bufferedReader.readLine();
+        ArrayList<Amenity> amenities = new ArrayList<Amenity>();
+        while (true) {
+            // <itemCode>,<description>,<price>,<floorArea>
+            String line = bufferedReader.readLine();
+            if (line == null) {
+                break;
+            }
+            String[] splitLine = line.split(",");
+            String itemCode = splitLine[0];
+            String description = splitLine[1];
+            String price = splitLine[2];
+            Double doublePrice = Double.parseDouble(price);
+            String floorArea = splitLine[3];
+            Double doubleFloorArea = Double.parseDouble(floorArea);
+            Amenity currAmenity = new InRoomAmenity(itemCode, description, doublePrice, doubleFloorArea);
+            amenities.add(currAmenity);
+        }
+
+        FileReader fileReader2 = new FileReader("SharedAmenity.txt");
+        BufferedReader bufferedReader2 = new BufferedReader(fileReader2);
+        bufferedReader2.readLine();
+        while (true) {
+            // <itemCode>,<description>,<price>
+            String line2 = bufferedReader2.readLine();
+            if (line2 == null) {
+                break;
+            }
+            String[] splitLine2 = line2.split(",");
+            String itemCode2 = splitLine2[0];
+            String description2 = splitLine2[1];
+            String price2 = splitLine2[2];
+            Double doublePrice2 = Double.parseDouble(price2);
+            Double doubleFloorArea2 = 0D;
+            Amenity currAmenity = new InRoomAmenity(itemCode2, description2, doublePrice2, doubleFloorArea2);
+            amenities.add(currAmenity);
+        }
+//        System.out.println(amenities);
+    }
+
+    public void saveRoomAvailability() throws IOException {
+        FileReader fileReader3 = new FileReader("rooms_april2023.txt");
+        BufferedReader bufferedReader3 = new BufferedReader(fileReader3);
+        bufferedReader3.readLine();
+        roomAvailability = new HashMap<Date,ArrayList<Integer>>();
+        while (true) {
+            // <itemCode>,<description>,<price>
+            String line3 = bufferedReader3.readLine();
+            if (line3 == null) {
+                break;
+            }
+            // <dd-mon-yyyy>,<standard rooms available>,<deluxe rooms available>
+            String[] splitLine3 = line3.split(",");
+            String dateString = splitLine3[0];
+            String standardRoomString = splitLine3[1];
+            Integer standardRoom = Integer.parseInt(standardRoomString);
+            String deluxeRoomString = splitLine3[2];
+            Integer deluxeRoom = Integer.parseInt(deluxeRoomString);
+            SimpleDateFormat format = new SimpleDateFormat("dd-MMM-yyyy");
+            Date date;
+            try {
+                date = format.parse(dateString);
+                ArrayList<Integer> standardDeluxeRoom = new ArrayList<Integer>();
+                standardDeluxeRoom.add(standardRoom);
+                standardDeluxeRoom.add(deluxeRoom);
+                roomAvailability.put(date, standardDeluxeRoom);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+        }
+//        System.out.println(roomAvailability);
     }
 }
